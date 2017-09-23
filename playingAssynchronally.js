3cecc191e53792c03491f586f35dcb3a22851e26
Marcelo Canaparro <marcelocanaparro@gmail.com>
2017-09-23 01:18:56 -0300

    updated exercises

--- playingAssynchronally.js
+++ playingAssynchronally.js
@@ -1,23 +1,35 @@
 var http = require('http')
+var bl = require('bl')
 var url = []
+
 url.push(process.argv[2])
 url.push(process.argv[3])
 url.push(process.argv[4])
-function teste(url) {
-    http.get(url[0], function(error, response) {
-        var responseConcat
-        if(error) {
-            return console.error(error)
-        }
-        url.pop()
-        if(url.length > 0) {
-            response.on('end',teste(url))
-        }
+
+var list = []
+var count = 0
+
+function printResults(list) {
+    list.forEach(function(element) {
+        console.log(element)
+    }, this);
+}
+
+function httpGet(index) {
+    http.get(url[index], function(response) {
         response.pipe(bl(function(error, data) {
             if(error){
                 return console.error(error)
             }
-            responseConcat = data.toString()
+            list[index] = data.toString()
+            count++
+            if(count === 3) {
+                printResults(list)
+            }
         }))
-    })
+    })    
+}
+
+for(let i = 0; i < 3; i++) {
+    httpGet(i)
 }
